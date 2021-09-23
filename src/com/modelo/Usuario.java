b7dbfd10d44d33d263700cdf6a96e331910bd3fa
mrknt33 <juan.rodriguez.m@estudiantes.utec.edu.uy>
2021-09-22 21:59:19 -0300

    Segundo

--- src/com/modelo/Usuario.java
+++ src/com/modelo/Usuario.java
@@ -21,4 +21,48 @@ public class Usuario {
         this.nombreUsuario = nombreUsuario;
         this.estatus = estatus;
     }
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public Estatus getEstatus() {
+		return estatus;
+	}
+	public void setEstatus(Estatus estatus) {
+		this.estatus = estatus;
+	}
+    
+    
 }
